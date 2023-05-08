6dd21bde070f06ea1c34c4cb1ec5032698c62279
GiuliaSetti <giulia.setti96@gmail.com>
2023-05-08 17:46:16 +0200

    feat: hello world visualized dynimically

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+
+    $hello = 'Hello';
+    $world = 'World!';
+
+    return view('home', compact('hello', 'world'));
 });
